c8890ce40ff774114ff4d2d13603768135373ec0
Sholzoma <59180316+Sholzoma@users.noreply.github.com>
2019-12-26 21:00:44 +0200

    Sholz character added

--- class_training.py
+++ class_training.py
@@ -1,2 +1,11 @@
 class Character:
-    pass
+    def __init__(self, name, level, race):
+        self.name = name
+        self.level = level
+        self.race = race
+    def __repr__(self):
+        return "Name: %s\nRace: %s\nLevel: %s"%(self.name, self.race, self.level)
+
+sholz = Character("Sholz", 1, "Elf")
+
+print(sholz)
